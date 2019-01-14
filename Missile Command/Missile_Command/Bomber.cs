2d4a7cd5ebca18ce4ef13c6015dee8d8296efc79
whatarethatguy <kenneth.chen.113@k12.friscoisd.org>
2019-01-14 12:10:32 -0600

    variables bomber

--- Missile Command/Missile_Command/Bomber.cs
+++ Missile Command/Missile_Command/Bomber.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace Missile_Command
 {
     public class bomber
-    {jalksdf;lksajmdlfk
-        public bomber(Texture2D t, )
+    {
+        public int points;
+        public int speed;
+        public int x;
+        public int y;
+        public int level;
+        public bomber(Texture2D t, int l)
+        {
+            level = l;
+
+        }
     }
 
 }
